8f1bd2df5e900fc4f7b96c3a9038c6c22a94aef5
Ean <45066060+Nam-SW@users.noreply.github.com>
2021-11-12 13:01:31 +0900

    refactor: 코드 구조 수정

---
+++ khaiii_utils.py
@@ -0,0 +1,90 @@
+import json
+import re
+
+from khaiii import KhaiiiApi
+
+SIGNIFICANT_TAGS = ["NNG", "NNP", "NNB", "VV", "VA", "VX", "MAG", "MAJ", "XSV", "XSA"]
+
+p1 = re.compile("[가-힣A-Za-z0-9]+/NN. [가-힣A-Za-z0-9]+/XS.")
+p2 = re.compile("[가-힣A-Za-z0-9]+/NN. [가-힣A-Za-z0-9]+/XSA [가-힣A-Za-z0-9]+/VX")
+p3 = re.compile("[가-힣A-Za-z0-9]+/VV")
+p4 = re.compile("[가-힣A-Za-z0-9]+/VX")
+
+
+def load_json(path):
+    with open(path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    return data
+
+
+def get_khaiii_api(rsc_dir):
+    return KhaiiiApi(rsc_dir=rsc_dir)
+
+
+def pos_text(texts, api):
+    corpus = []
+    for sent in texts:
+        pos_tagged = ""
+        for word in api.analyze(sent):
+            for morph in word.morphs:
+                if morph.tag in SIGNIFICANT_TAGS:
+                    pos_tagged += morph.lex + "/" + morph.tag + " "
+        corpus.append(pos_tagged.strip())
+    return corpus
+
+
+def stemming_text(text):
+    corpus = []
+    for sent in text:
+        ori_sent = sent
+        mached_terms = re.findall(p1, ori_sent)
+        for terms in mached_terms:
+            ori_terms = terms
+            modi_terms = ""
+            for term in terms.split(" "):
+                lemma = term.split("/")[0]
+                tag = term.split("/")[-1]
+                modi_terms += lemma
+            modi_terms += "다/VV"
+            ori_sent = ori_sent.replace(ori_terms, modi_terms)
+
+        mached_terms = re.findall(p2, ori_sent)
+        for terms in mached_terms:
+            ori_terms = terms
+            modi_terms = ""
+            for term in terms.split(" "):
+                lemma = term.split("/")[0]
+                tag = term.split("/")[-1]
+                if tag != "VX":
+                    modi_terms += lemma
+            modi_terms += "다/VV"
+            ori_sent = ori_sent.replace(ori_terms, modi_terms)
+
+        mached_terms = re.findall(p3, ori_sent)
+        for terms in mached_terms:
+            ori_terms = terms
+            modi_terms = ""
+            for term in terms.split(" "):
+                lemma = term.split("/")[0]
+                tag = term.split("/")[-1]
+                modi_terms += lemma
+            if "다" != modi_terms[-1]:
+                modi_terms += "다"
+            modi_terms += "/VV"
+            ori_sent = ori_sent.replace(ori_terms, modi_terms)
+
+        mached_terms = re.findall(p4, ori_sent)
+        for terms in mached_terms:
+            ori_terms = terms
+            modi_terms = ""
+            for term in terms.split(" "):
+                lemma = term.split("/")[0]
+                tag = term.split("/")[-1]
+                modi_terms += lemma
+            if "다" != modi_terms[-1]:
+                modi_terms += "다"
+            modi_terms += "/VV"
+            ori_sent = ori_sent.replace(ori_terms, modi_terms)
+        corpus.append(ori_sent)
+    return corpus
